afd86c72eaf922ae4bd61e1fc45d5b07a0ae2331
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 00:36:14 +0900

    Update streamlit_app.py

--- my_app/streamlit_app.py
+++ my_app/streamlit_app.py
@@ -3,3 +3,29 @@ my_app
 └── pages
     ├── page_2.py       <-- New page 2!
     └── page_3.py       <-- New page 3!
+
+    
+    
+# Contents of ~/my_app/streamlit_app.py
+import streamlit as st
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
