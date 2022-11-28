ce83d29f3af7a7ebaa9e48d1e2c653a1eeab6381
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 01:19:24 +0900

    Update main_page.py

--- my_app/pages/main_page.py
+++ my_app/pages/main_page.py
@@ -9,14 +9,9 @@ def page2():
     st.markdown("# Page 2 ❄️")
     st.sidebar.markdown("# Page 2 ❄️")
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
 page_names_to_funcs = {
     "Main Page": main_page,
     "Page 2": page2,
-    "Page 3": page3,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
