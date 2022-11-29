baade01f85a9ddec0d645b9df308c2e3db0726b7
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 11:22:26 +0900

    Update Airline_passenger_satisfaction.py

--- my_app/Airline_passenger_satisfaction.py
+++ my_app/Airline_passenger_satisfaction.py
@@ -5,15 +5,19 @@ def main_page():
     st.markdown("# Airline passenger satisfaction")
     st.sidebar.markdown("# Airline passenger satisfaction")
 
-def page():
+def Practice():
+    st.markdown("# Practice")
+    st.sidebar.markdown("# Page 2 ❄️")
+    
+def APP():
     st.markdown("# Practice")
     st.sidebar.markdown("# Page 2 ❄️")
 
 
-
 page_names_to_funcs = {
-    "main_page": practice,
-     "page": APP,
+    "main_page": Airline_passenger_satisfaction,
+    "Practice": Practice,
+    "APP": APP,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
