afbaa35bec57860419af6aba828b7c1b079140b7
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 12:10:54 +0900

    Update Airline_passenger_satisfaction.py

--- my_app/Airline_passenger_satisfaction.py
+++ my_app/Airline_passenger_satisfaction.py
@@ -1,19 +1,19 @@
 # Contents of ~/my_app/Airline_passenger_satisfaction.py
 import streamlit as st
 
-# # 필요 라이브러리 import
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import streamlit as st
-# import kmeans
-# import prepro
-# import logic
-# import tree
-# import forest
-# import xg
-# import knn
-# import joblib
-# from PIL import Image
+# 필요 라이브러리 import
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import kmeans
+import prepro
+import logic
+import tree
+import forest
+import xg
+import knn
+import joblib
+from PIL import Image
 
 # streamlit 앱 제목
 st.title("항공사 고객 만족도 Machine Learning")
