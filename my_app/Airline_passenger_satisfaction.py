5114d794955a72256de3ee01fcc34213917f8c64
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 11:24:14 +0900

    Update Airline_passenger_satisfaction.py

--- my_app/Airline_passenger_satisfaction.py
+++ my_app/Airline_passenger_satisfaction.py
@@ -1,24 +1,24 @@
 # Contents of ~/my_app/Airline_passenger_satisfaction.py
 import streamlit as st
 
-def main_page():
-    st.markdown("# Airline passenger satisfaction")
-    st.sidebar.markdown("# Airline passenger satisfaction")
+#def main_page():
+#    st.markdown("# Airline passenger satisfaction")
+#    st.sidebar.markdown("# Airline passenger satisfaction")
 
-def Practice():
-    st.markdown("# Practice")
-    st.sidebar.markdown("# Page 2 ❄️")
+#def Practice():
+#    st.markdown("# Practice")
+#    st.sidebar.markdown("# Page 2 ❄️")
     
-def APP():
-    st.markdown("# Practice")
-    st.sidebar.markdown("# Page 2 ❄️")
+#def APP():
+#    st.markdown("# Practice")
+#    st.sidebar.markdown("# Page 2 ❄️")
 
 
-page_names_to_funcs = {
-    "main_page": Airline_passenger_satisfaction,
-    "Practice": Practice,
-    "APP": APP,
-}
+#page_names_to_funcs = {
+#    "main_page": Airline_passenger_satisfaction,
+#    "Practice": Practice,
+#    "APP": APP,
+#}
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+#selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+#page_names_to_funcs[selected_page]()
