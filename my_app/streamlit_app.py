7765edabe7264cffc44de9bede5e00a778b2cb43
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 09:58:16 +0900

    Update streamlit_app.py

--- my_app/streamlit_app.py
+++ my_app/streamlit_app.py
@@ -118,8 +118,7 @@ st.markdown('**<center><span style="color: MidnightBlue; font-size:250%">Thank Y
 
 page_names_to_funcs = {
     "Main Page": main_page,
-#     "Page 2": page2,
-#     "Page 3": page3,
+     "Page 2": page2,
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
